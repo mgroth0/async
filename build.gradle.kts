4b388d27b0f3a37ff9d47c4ce08d5dcbc3320201
mgroth0 <mgroth49@gmail.com>
2022-07-06 12:59:02 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ modtype = LIB
 
 dependencies {
   implementation(projects.k.kjlib.lang)
-  implementation(projects.k.kjlib)
   api(libs.kotlinx.serialization.json)
   api(libs.kotlinx.coroutines)
 }
