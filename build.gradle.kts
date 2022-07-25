d3d613e94b4b33a5738217462f62a01716b426c4
mgroth0 <mgroth49@gmail.com>
2022-07-25 01:33:34 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-apis(
-  libs.kotlinx.serialization.json,
-  libs.kotlinx.coroutines
-)
 
-implementations(
-  projects.k.klib,
-  ":k:file".jvm(),
-  projects.k.kjlib.lang
-)
 
-plugins {
-  kotlin("plugin.serialization")
+implementations {
+  klib
+  file
+  kjlibLang
+}
+
+apis {
+  libs.`kotlinx-serialization-json`
+  libs.`kotlinx-coroutines`
 }
