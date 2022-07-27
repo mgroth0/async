4d2112bc1800db01e27a662183c3fc542a6cddde
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-26 22:04:52 -0400

    autocommit

--- gpu/build.gradle.kts
+++ gpu/build.gradle.kts
@@ -5,18 +5,8 @@ apis(
   ":k:klib".jvm()
 )
 dependencies {
-//  implementation(projects.k.kjlib.lang)
-//  implementation(projects.k.kjlib)
-
   implementation(libs.aparapi)
   /*implementation(libs.aparapi)*/
-
-  /*api("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
-
-
-
 
   /*println("testing if newMac")*/
   /*    if (isNewMac) {
@@ -39,37 +29,4 @@ dependencies {
 	  } else {*/
   implementation(libs.aparapi)
   /*}*/
-
-  /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
-  /*implementation(
-	files(
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-api-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-api-0.4.0-SNAPSHOT-macosx-arm64.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-generator-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-platform-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-framework-0.4.0-SNAPSHOT.jar",
-	)
-  )*/
-
-
 }
-
-/*
-configurations.all {
-  resolutionStrategy.dependencySubstitution {
-	substitute(module("org.tensorflow:tensorflow-core-api"))
-	  .using(module("org.tensorflow:tensorflow-core-api:0.4.0"))
-	  .withClassifier("macosx-x86_64")
-  }
-}*/
-
-
-/*
-configurations.all {
-  resolutionStrategy.dependencySubstitution {
-    substitute(module("org.tensorflow:tensorflow-core-api"))
-      .using(module("org.tensorflow:tensorflow-core-api:0.4.0"))
-      .withClassifier("macosx-x86_64")
-  }
-}*/
