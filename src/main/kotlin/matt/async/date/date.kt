251c095c2a3bf841052ab21025822285eace883f
mgroth0 <mgroth49@gmail.com>
2022-09-03 11:17:46 -0400

    autocommit

--- src/main/kotlin/matt/async/date/date.kt
+++ src/main/kotlin/matt/async/date/date.kt
@@ -3,27 +3,12 @@ package matt.async.date
 import matt.async.safe.with
 import matt.async.schedule.every
 import matt.klib.dmap.withStoringDefault
-import matt.klib.lang.RUNTIME_MX
-import matt.klib.math.BILLION
-import matt.klib.math.MILLION
-import matt.klib.math.THOUSAND
 import matt.klib.math.mean
 import matt.klib.math.median
-import matt.klib.math.roundToDecimal
 import matt.klib.str.addSpacesUntilLengthIs
 import matt.klib.str.tab
+import matt.time.dur.Duration
 import java.io.PrintWriter
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.OffsetDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
-import java.util.Date
 import java.util.concurrent.Semaphore
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind.EXACTLY_ONCE
@@ -32,69 +17,9 @@ import kotlin.contracts.contract
 var simplePrinting = false
 
 
-
-val Number.unixSeconds: Date
-  get() = Date((this.toDouble()*1000).toLong())
-val Number.unixMS: Date
-  get() = Date(this.toLong())
-
-
-private val stupid = "Have to keep it as a different name than Duration.format since they are in the same package???"
-
-const val myDateFormatStr = "EEE, MMM d, h:mm a"
-val myDateTimeFormat: DateTimeFormatter = DateTimeFormatter.ofPattern(myDateFormatStr)
-fun Date.formatDate(): String = SimpleDateFormat(myDateFormatStr).format(this)
-fun today(): LocalDate = LocalDate.now()
-fun tomorrow(): LocalDate = today().plus(1, ChronoUnit.DAYS)
-fun nowDateTime(): LocalDateTime = today().atTime(LocalTime.now())
-
-fun localDateTimeOfEpochMilli(ms: Long): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(ms), ZoneId.systemDefault())
-
-fun milli() = System.currentTimeMillis()
-
-fun LocalDateTime.atTime(hour: Int, min: Int): LocalDateTime = toLocalDate().atTime(hour, min)
-private val OFFSET: ZoneOffset = OffsetDateTime.now().offset
-fun LocalDateTime.toEpochMilli() = toEpochSecond(OFFSET)*1000
-
-operator fun Date.minus(started: Date): Duration {
-  return this.toInstant() - started.toInstant()
-}
-
-operator fun Instant.minus(started: Instant): Duration {
-  return (this.epochSecond - started.epochSecond).sec
-}
-
-operator fun Date.minus(started: Instant): Duration {
-  return toInstant() - started
-}
-
-operator fun Instant.minus(started: Date): Duration {
-  return this - started.toInstant()
-}
-
 fun println_withtime(s: String) {
   println(System.currentTimeMillis().toString() + ":" + s)
 }
-
-val Number.nanos
-  get() = Duration.ofNanoseconds(this)
-val Number.ms
-  get() = Duration.ofMilliseconds(this)
-val Number.sec
-  get() = Duration.ofSeconds(this)
-val Number.min
-  get() = Duration.ofMinutes(this)
-val Number.hours
-  get() = Duration.ofHours(this)
-val Number.hour
-  get() = hours
-val Number.days
-  get() = Duration.ofDays(this)
-val Number.day
-  get() = days
-
-fun now() = System.currentTimeMillis().unixMS
-
 
 @ExperimentalContracts
 fun <R> stopwatch(s: String, op: ()->R): R {
