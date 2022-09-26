f7b67860104815b3f6683c40cbbc12d36113cc88
mgroth0 <mgroth49@gmail.com>
2022-09-25 23:43:15 -0400

    autocommit

--- src/main/kotlin/matt/async/thread/thread.kt
+++ src/main/kotlin/matt/async/thread/thread.kt
@@ -21,7 +21,7 @@ fun <R> R.runInDaemon(op: R.()->Unit) {
 }
 
 
-inline fun daemon(crossinline block: ()->Unit): Thread {
+fun daemon(block: ()->Unit): Thread {
   return thread(isDaemon = true) {
 	block()
   }
