3d754cb3f8bdec6f6d84511a46052d080ae789de
mgroth0 <mgroth49@gmail.com>
2022-10-11 11:00:16 -0400

    autocommit

--- src/jvmMain/kotlin/matt/async/schedule/schedule.kt
+++ src/jvmMain/kotlin/matt/async/schedule/schedule.kt
@@ -5,8 +5,8 @@ import matt.async.schedule.ThreadInterface.Canceller
 import matt.async.thread.daemon
 import matt.collect.maxlist.MaxList
 import matt.lang.massert
-import matt.log.Logger
 import matt.log.NONE
+import matt.log.logger.Logger
 import matt.model.latch.SimpleLatch
 import matt.model.valjson.ValJson
 import matt.time.UnixTime
