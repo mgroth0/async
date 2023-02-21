2574299709479dc15aea4aa8b35c30b1131702f6
mgroth0 <mgroth49@gmail.com>
2023-02-21 17:47:19 -0500

    autocommit

--- src/jvmMain/kotlin/matt/async/timeout/timeout.kt
+++ src/jvmMain/kotlin/matt/async/timeout/timeout.kt
@@ -2,8 +2,10 @@ package matt.async.timeout
 
 import matt.async.thread.daemon
 import matt.lang.function.Op
+import matt.model.code.errreport.ThrowReport
 import matt.model.flowlogic.latch.SimpleLatch
 import matt.time.dur.sleep
+import java.lang.Thread.UncaughtExceptionHandler
 import kotlin.time.Duration
 
 /*
@@ -28,13 +30,13 @@ fun timeoutDaemon(timeout: Duration, op: Op): Thread {
   var timeoutThread: Thread? = null
   val timeoutThreadInitialized = SimpleLatch()
   val mainThreadStarted = SimpleLatch()
-  val mainThread = daemon {
+  val mainThread = calmWhenInterruptedDaemon {
 	mainThreadStarted.open()
 	op()
 	timeoutThreadInitialized.await()
 	timeoutThread!!.interrupt()
   }
-  timeoutThread = daemon {
+  timeoutThread = calmWhenInterruptedDaemon {
 	sleep(timeout)
 	mainThreadStarted.await()
 	if (mainThread.isAlive) mainThread.interrupt()
@@ -43,5 +45,34 @@ fun timeoutDaemon(timeout: Duration, op: Op): Thread {
   return mainThread
 }
 
+fun calmWhenInterruptedDaemon(op: Op): Thread {
+  var d: Thread? = null
+  val theOp = {
+	try {
+	  op()
+	} catch (e: InterruptedException) {
+	  /*this is the new way, and prevents a warning symbol through Gradle Tooling saying "unexpected exception"*/
+	  /*wait... the errors gradle may have been talking about may have been the configuration cache errors...*/
+	  println("$d was interrupted")
+	}
+  }
+  d = daemon(start = false) {
+	theOp()
+  }
+  d.uncaughtExceptionHandler = MellowInterruptsHandler
+  d.start()
+  return d
+}
 
+/*this was the old way*/
+object MellowInterruptsHandler: UncaughtExceptionHandler {
+  override fun uncaughtException(t: Thread?, e: Throwable?) {
+	if (e is InterruptedException) {
+	  println("$t was interrupted")
+	} else {
+	  ThrowReport(t, e).print()
+	}
+  }
+
+}
 
