547ca000d6709b1a8bd9cd89baa3f0fbc3b977f2
mgroth0 <mgroth49@gmail.com>
2023-03-27 15:12:33 -0400

    autocommit

--- src/commonMain/kotlin/matt/async/collect/map/proxy/proxy.kt
+++ src/commonMain/kotlin/matt/async/collect/map/proxy/proxy.kt
@@ -10,67 +10,86 @@ import matt.async.collect.map.SuspendMutableMap
 import matt.async.collect.map.toFakeSuspendMutableEntry
 import matt.async.collect.set.SuspendMutableSet
 import matt.async.collect.set.fake.toSuspendingFakeMutableSet
+import matt.model.data.proxy.map.ImmutableProxyMap
 import matt.model.op.convert.Converter
 
 
-fun <SK: Any, SV: Any, TK: Any, TV: Any> SuspendMutableMap<SK, SV>.proxy(
-  keyConverter: Converter<SK, TK>,
-  valueConverter: Converter<SV, TV>
-) = SuspendProxyMap(this, keyConverter, valueConverter)
+fun <SK : Any, SV : Any, TK : Any, TV : Any> SuspendMutableMap<SK, SV>.proxy(
+    keyConverter: Converter<SK, TK>,
+    valueConverter: Converter<SV, TV>
+) = SuspendProxyMap(
+    this,
+    keyConverter,
+    valueConverter
+)
 
-class SuspendProxyMap<SK: Any, SV: Any, TK: Any, TV: Any>(
-  private val innerMap: SuspendMutableMap<SK, SV>,
-  private val keyConverter: Converter<SK, TK>,
-  private val valueConverter: Converter<SV, TV>
-): SuspendMutableMap<TK, TV> {
+class SuspendProxyMap<SK : Any, SV : Any, TK : Any, TV : Any>(
+    private val innerMap: SuspendMutableMap<SK, SV>,
+    private val keyConverter: Converter<SK, TK>,
+    private val valueConverter: Converter<SV, TV>
+) : SuspendMutableMap<TK, TV> {
 
-  private fun SK.toTK() = keyConverter.convertToB(this)
-  private fun SV.toTV() = valueConverter.convertToB(this)
-  private fun TK.toSK() = keyConverter.convertToA(this)
-  private fun TV.toSV() = valueConverter.convertToA(this)
+    private fun SK.toTK() = keyConverter.convertToB(this)
+    private fun SV.toTV() = valueConverter.convertToB(this)
+    private fun TK.toSK() = keyConverter.convertToA(this)
+    private fun TV.toSV() = valueConverter.convertToA(this)
 
-  override suspend fun entries(): SuspendMutableSet<out SuspendMutableEntry<TK, TV>> {
+    override suspend fun entries(): SuspendMutableSet<out SuspendMutableEntry<TK, TV>> {
 
-	return innerMap.entries().map { e ->
-	  object: SuspendEntry<TK, TV> {
-		override suspend fun key() = e.key().toTK()
-		override suspend fun value() = e.value().toTV()
-	  }.toFakeSuspendMutableEntry()
-	}.toSet().toSuspendingFakeMutableSet()
-
-
-  }
+        return innerMap.entries().map { e ->
+            object : SuspendEntry<TK, TV> {
+                override suspend fun key() = e.key().toTK()
+                override suspend fun value() = e.value().toTV()
+            }.toFakeSuspendMutableEntry()
+        }.toSet().toSuspendingFakeMutableSet()
 
 
-  override suspend fun keys(): SuspendMutableSet<TK> = entries().map { it.key() }.toSet().toSuspendingFakeMutableSet()
-  override suspend fun size(): Int = innerMap.size()
-  override suspend fun values(): SuspendMutableCollection<TV> = entries().map { it.value() }.toSuspendingFakeMutableList()
+    }
 
-  override suspend fun clear() = innerMap.clear()
 
-  override suspend fun isEmpty() = innerMap.isEmpty()
+    override suspend fun keys(): SuspendMutableSet<TK> = entries().map { it.key() }.toSet().toSuspendingFakeMutableSet()
+    override suspend fun size(): Int = innerMap.size()
+    override suspend fun values(): SuspendMutableCollection<TV> =
+        entries().map { it.value() }.toSuspendingFakeMutableList()
 
-  override suspend fun remove(key: TK) = innerMap.remove(key.toSK())?.toTV()
+    override suspend fun clear() = innerMap.clear()
 
-  override suspend fun putAll(from: Map<out TK, TV>) {
-	innerMap.putAll(from.mapKeys { it.key.toSK() }.mapValues { it.value.toSV() })
-  }
+    override suspend fun isEmpty() = innerMap.isEmpty()
+    override suspend fun toNonSuspendMap(): Map<TK, TV> {
+        return ImmutableProxyMap(
+            innerMap.toNonSuspendMap(),
+            keyConverter,
+            valueConverter
+        )
+    }
 
-  override suspend fun put(key: TK, value: TV): TV? {
-	return innerMap.put(key.toSK(), value.toSV())?.toTV()
-  }
+    override suspend fun remove(key: TK) = innerMap.remove(key.toSK())?.toTV()
 
-  override suspend fun get(key: TK): TV? {
-	return innerMap.get(key.toSK())?.toTV()
-  }
+    override suspend fun putAll(from: Map<out TK, TV>) {
+        innerMap.putAll(from.mapKeys { it.key.toSK() }.mapValues { it.value.toSV() })
+    }
 
-  override suspend fun containsValue(value: TV): Boolean {
-	return innerMap.containsValue(value.toSV())
-  }
+    override suspend fun put(
+        key: TK,
+        value: TV
+    ): TV? {
+        return innerMap.put(
+            key.toSK(),
+            value.toSV()
+        )?.toTV()
+    }
 
-  override suspend fun containsKey(key: TK): Boolean {
-	return innerMap.containsKey(key.toSK())
-  }
+    override suspend fun get(key: TK): TV? {
+        return innerMap.get(key.toSK())?.toTV()
+    }
+
+    override suspend fun containsValue(value: TV): Boolean {
+        return innerMap.containsValue(value.toSV())
+    }
+
+    override suspend fun containsKey(key: TK): Boolean {
+        return innerMap.containsKey(key.toSK())
+    }
 
 
 }
