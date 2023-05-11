91b5a8005da52c89e16d1baf26856c5996d86245
mgroth0 <mgroth49@gmail.com>
2023-05-11 19:57:43 -0400

    changed src/commonMain/kotlin/matt/async/lock/sem/sem.kt

--- src/commonMain/kotlin/matt/async/lock/sem/sem.kt
+++ src/commonMain/kotlin/matt/async/lock/sem/sem.kt
@@ -8,51 +8,49 @@ import kotlin.contracts.contract
 
 
 class ReadWriteSem {
-  private val WRITE_PERMIT = 1000
-  private val sem = Semaphore(WRITE_PERMIT, 0)
-  suspend fun <R> withReadPermit(key: ReentranceKey? = null, op: suspend (ReentranceKey)->R): R {
-	return if (key == null) sem.withPermit {
-	  op(ReentranceKey())
-	} else {
-	  op(ReentranceKey())
-	}
-  }
+    private val WRITE_PERMIT = 1000
+    private val sem = Semaphore(WRITE_PERMIT, 0)
+    suspend fun <R> withReadPermit(key: ReentranceKey? = null, op: suspend (ReentranceKey) -> R): R {
+        return if (key == null) sem.withPermit {
+            op(ReentranceKey())
+        } else op(ReentranceKey())
+    }
 
-  suspend fun <R> withWritePermit(key: ReentranceKey? = null, op: suspend (ReentranceKey)->R): R {
-	return if (key == null) sem.withPermits(WRITE_PERMIT) {
-	  op(ReentranceKey())
-	} else {
-	  op(ReentranceKey())
-	}
-  }
+    suspend fun <R> withWritePermit(key: ReentranceKey? = null, op: suspend (ReentranceKey) -> R): R {
+        return if (key == null) sem.withPermits(WRITE_PERMIT) {
+            op(ReentranceKey())
+        } else {
+            op(ReentranceKey())
+        }
+    }
 
-  inner class ReentranceKey
+    inner class ReentranceKey
 }
 
 
 suspend fun Semaphore.acquireN(count: Int) {
-  repeat(count) {
-	acquire()
-  }
+    repeat(count) {
+        acquire()
+    }
 }
 
 fun Semaphore.releaseN(count: Int) {
-  repeat(count) {
-	release()
-  }
+    repeat(count) {
+        release()
+    }
 }
 
 
 @OptIn(ExperimentalContracts::class)
-suspend inline fun <T> Semaphore.withPermits(n: Int, action: ()->T): T {
-  contract {
-	callsInPlace(action, EXACTLY_ONCE)
-  }
+suspend inline fun <T> Semaphore.withPermits(n: Int, action: () -> T): T {
+    contract {
+        callsInPlace(action, EXACTLY_ONCE)
+    }
 
-  acquireN(n)
-  try {
-	return action()
-  } finally {
-	releaseN(n)
-  }
+    acquireN(n)
+    try {
+        return action()
+    } finally {
+        releaseN(n)
+    }
 }
